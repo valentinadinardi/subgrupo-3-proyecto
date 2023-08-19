81e21ad22c167009726b4a0eaac892c0edde5344
narce2898 <natyarce2014@gmail.com>
2023-08-19 01:17:17 -0300

    Avance
    Precios, vendidos, título y subtítulo

--- js/products.js
+++ js/products.js
@@ -1,55 +1,45 @@
 //array donde se cargarán los datos recibidos:
 let categoriesArray = [];
 
-
 // URL de la API que contiene la colección de productos en formato JSON
 const apiUrl = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
-
 const productListElement = document.getElementById("product-list-container");
 
-
-
-
-//función que recibe un array con los datos, y los muestra en pantalla a través el uso del DOM
+//función que recibe un array con los datos, y los muestra en pantalla a través del uso del DOM
 function showCategoriesList(array){
     let htmlContentToAppend = "";
 
     for(let i = 0; i < array.length; i++){ 
         let products = array[i];
-        console.log(array[i])
 
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
-        
                 <div class="col-3">
-                    <img src="` + products.image + `" alt="product image" class="img-thumbnail">
+                    <img src="${products.image}" alt="product image" class="img-thumbnail">
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <div class="mb-1">
-                        <h4>`+ products.name +`</h4> 
-                        <p> `+ products.description +`</p> 
+                            <h4>${products.name} - USD: ${products.cost.toLocaleString()}</h4>
+                            <p>${products.description}</p>
                         </div>
-                        <small class="text-muted">` +products.soldCount  + ` artículos</small> 
+                        <small class="text-muted">${products.soldCount} vendidos</small>
                     </div>
-
                 </div>
             </div>
-        </div>
-        `
-        document.getElementById("product-list-container").innerHTML = htmlContentToAppend; 
+        </div>`;
     }
-}
 
+    productListElement.innerHTML = htmlContentToAppend; 
+}
 
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(apiUrl).then(function(resultObj){
         if (resultObj.status === "ok")
         {
             categoriesArray = resultObj.data.products;
-            console.log(categoriesArray)
             showCategoriesList(categoriesArray);
         }
     });
