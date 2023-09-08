2f54188e67100b71320cb5ff145e89459e663636
Elias Garcia <129087002+elias-garcia-0@users.noreply.github.com>
2023-09-08 15:24:16 -0300

    Estilos de product-info.html

--- js/product-info.js
+++ js/product-info.js
@@ -27,19 +27,38 @@ document.addEventListener("DOMContentLoaded", function () {
           // Mostramos la información del producto en el HTML que para eso añadimos el container
           const productInfoContainer = document.getElementById("product-info-container");
           productInfoContainer.innerHTML = `
-            <h3>${productName}</h3>
-            <p> Precio: ${productCurrency} ${productCost} </p>
-            <p> Descripción: ${productDescription}</p>
-            <p> Categoría: ${productCategory}</p>
-            <p> Cantidad de vendidos: ${productSoldCount}</p>
-            <p> Imágenes ilustrativas</p>
-            <ul>
-              ${productImages.map(image => `<li><img src="${image}" alt="Imagen del Producto"></li>`).join("")}
-            </ul>
-            <h4>Productos Relacionados</h4>
-            <ul>
-              ${relatedProducts.map(relatedProduct => `<li>${relatedProduct.name}</li>`).join("")}
-            </ul>
+            <h1 class="text-start">${productName}</h1>
+            <hr/>
+            <p class="text-start"><strong> Precio:</strong></p>
+            <p class="text-start"> ${productCurrency} ${productCost} </p>
+
+            <p class="text-start"><strong> Descripción:</strong></p>
+            <p class="text-start"> ${productDescription}</p>
+
+            <p class="text-start"><strong> Categoría:</strong></p>
+            <p class="text-start">${productCategory}</p>
+
+            <p class="text-start"><strong> Cantidad de vendidos:</strong></p>
+            <p class="text-start">${productSoldCount}</p>
+
+            <p class="text-start"><strong> Imágenes ilustrativas </strong></p>
+            
+            <div>
+              <h4>Imágenes del Producto</h4>
+                 <div class="row">
+                    ${productImages.map(image => `
+                    <div class="col-md-3">
+                      <img src="${image}" alt="Imagen del Producto" class="img-fluid">
+                    </div>
+                    `).join("")}
+                  </div>
+
+                  <h4>Productos Relacionados</h4>
+                   <ul>
+                     ${relatedProducts.map(relatedProduct => `<li>${relatedProduct.name}</li>`).join("")}
+                   </ul>
+            </div>
+
           `;
         })
         .catch(function (error) {
