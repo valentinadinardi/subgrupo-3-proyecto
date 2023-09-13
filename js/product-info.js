7b7c150c32f94f777484a9f30ceab393306682a3
valentinadinardi <138677905+valentinadinardi@users.noreply.github.com>
2023-09-13 10:15:54 -0300

    Trabajo finalizado
    Agregamos la sección para comentar.
    Se agrega el comentario al resto de los comentarios.

--- js/product-info.js
+++ js/product-info.js
@@ -122,8 +122,39 @@ document.addEventListener("DOMContentLoaded", function () {
   } else {
     console.error("No se encontró un ID de producto en el almacenamiento local.");
   }
-});
 
+let btnComentar = document.getElementById("btnComentar");
+
+btnComentar.addEventListener("click", function (){
+ let comentarios = document.getElementById("nuevosComentarios");
+
+let nuevoComentario = document.getElementById("comentar").value;
+let usuario = localStorage.getItem("userName")
+let calificacionNumero = document.getElementById("puntuaciones").value;
+let calificacionEstrellas = generateStars(calificacionNumero);
+
+// crea un nuevo objeto `Date`
+let today = new Date()
+
+ // obtener la fecha y la hora
+var nowFecha = today.toISOString().split('T')[0]
+var nowHora = today.toLocaleTimeString();
+
+
+let comentariosNuevos = "";
+comentariosNuevos += `
+<hr>
+<ul>
+    <li>
+        <p>${usuario} ${nowFecha} ${nowHora} ${calificacionEstrellas}</p>
+        <p>${nuevoComentario}</p>
+    </li>
+</ul>
+`;
+
+comentarios.innerHTML = comentariosNuevos;
+
+})
 
 
 //Obtener el item definido en login.js con la key userName
@@ -136,6 +167,7 @@ let mostrarUser = document.getElementsByClassName("user");
 for (let i = 0; i < mostrarUser.length; i++) {
   mostrarUser[i].textContent = storedData;
 }
+});
 
 //join("") se utiliza para unir todos los fragmentos HTML en una sola cadena de texto.//
 //map es un método de JavaScript que se utiliza para iterar sobre cada elemento del array//
